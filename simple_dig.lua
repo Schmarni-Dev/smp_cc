14fcc32b1323421ce366c944d7ef5f75cfe0cfb8
Schmarni <marnistromer@gmail.com>
2024-06-23 17:20:40 +0200

    add simple_dig by domi
    Signed-off-by: Schmarni <marnistromer@gmail.com>

---
+++ simple_dig.lua
@@ -0,0 +1,25 @@
+for i = 1, 128, 1 do
+	turtle.forward()
+	turtle.dig()
+end
+turtle.turnRight()
+turtle.forward()
+turtle.turnRight()
+for i = 1, 128, 1 do
+	turtle.forward()
+	turtle.dig()
+end
+turtle.turnLeft()
+turtle.forward()
+turtle.turnLeft()
+for i = 1, 128, 1 do
+	turtle.forward()
+	turtle.dig()
+end
+turtle.turnRight()
+turtle.forward()
+turtle.turnRight()
+for i = 1, 128, 1 do
+	turtle.forward()
+	turtle.dig()
+end
