7fdd17e99c45e8e40295c97a2e4e3c4942c22076
domi <dominikpatzner1@gmail.com>
2025-02-05 18:45:15 +0100

    new update

--- tree_farm_place.lua
+++ tree_farm_place.lua
@@ -1,14 +1,24 @@
 rednet.open("left")
 rednet.host("Farm1", "Sapling_Turtle1")
+rednet.host("farm_block_detail1", "Sapling_Turtle1")
+local last_item
 while true do
     local Block, information = turtle.inspect()
     if Block and information.tags and information.tags["minecraft:logs"] then
         rednet.broadcast("1", "Farm1")
-        -- turtle.dig()
-        -- turtle.drop()
     end
     if not Block then
         turtle.suckDown(1)
+        local detail = turtle.getItemDetail()
+        if detail and detail.name ~= last_item then
+            if detail.name == "minecraft:crimson_fungus" or detail.name == "minecraft:warped_fungus" then
+                rednet.broadcast("nylium", "farm_block_detail1")
+            else
+                rednet.broadcast("dirt", "farm_block_detail1")
+            end
+            rednet.receive("farm_block_done1")
+            last_item = detail.name
+        end
         turtle.place()
     end
 end
